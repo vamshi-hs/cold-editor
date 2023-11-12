ed78e712a9738634a946689f51bebe58133f7de5
Vamshi Krishna <vamshi5070k@gmail.com>
2023-11-12 22:01:07 +0530

    completed entering raw mode section

--- src/main.c
+++ src/main.c
@@ -3,16 +3,23 @@
 #include <ctype.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode() {
-
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -23,7 +30,7 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 
 }
 
@@ -34,7 +41,7 @@ int main(){
 
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
       /* == 1 && (c != 'q')){ */
     if (iscntrl(c)){
       printf("%d\r\n", c);
